e32c2616a185475a86784226d58b86955bf078b3
malongshu <malongshu@sensefun.com>
2017-07-18 12:34:03 +0800

    Add        .dockerignore Add        Dockerfile Add        docker-entrypoint.sh Add        ext/swoole.ini Add        ext/swoole.so Add        ext/yaf.ini Add        ext/yaf.so Add        ext/zookeeper.ini Add        ext/zookeeper.so Add        sources.list Add        www.conf Add        www.conf.default

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "changing directory permssion..."
+for dir in `mount|awk '{print \$3}'`;do
+   if [[ $dir =~ ^/var/www/html/ ]]; then
+	cmd="chmod -R a+rw $dir";
+	echo $cmd;
+   	$cmd;
+   fi
+done	
+
+echo
+for f in /docker-entrypoint.d/*; do
+	case "$f" in
+		*.sh) echo "$0: running $f"; . "$f" ;;
+		*)    echo "$0: ignoring $f" ;;
+	esac
+	echo
+done
+
+exec "$@"
